8add9896245daaa41e7a01d127976c8d6bce534e
Cory Schmunsler <schmunsler@gmail.com>
2013-04-20 21:34:06 -0500

    Added avatar.animate.user.js

---
+++ avatar.animate.user.js
@@ -0,0 +1,18 @@
+// Save this script in a file named "avatar.animate.user.js"
+// and drag'n'drop on chrome://extensions/ to install.
+// (for Firefox use Greasemonkey)
+// Released under WTFPL (http://www.wtfpl.net/txt/copying/)
+
+// ==UserScript==
+// @name G+ Animated Avatars
+// @description Animate avatars on Google+
+// @match http://plus.google.com/*
+// @match https://plus.google.com/*
+// ==/UserScript==
+
+window.addEventListener('load', function() {
+   var images = document.getElementsByTagName('img');
+   for(i=0; i<images.length; i++) {
+      images[i].src = images[i].src.replace(/googleusercontent\.com(.*)s48-c-k/, "googleusercontent\.com$1s48-c");
+   }
+}, false);
