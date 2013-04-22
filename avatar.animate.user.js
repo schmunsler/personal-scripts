9020b6b1b9265ead513f9a59017e7d4b0a9cb1d7
Cory Schmunsler <schmunsler@gmail.com>
2013-04-22 00:16:53 -0500

    Make match pattern configurable to allow exceptions.

--- avatar.animate.user.js
+++ avatar.animate.user.js
@@ -3,6 +3,9 @@
 // (for Firefox use Greasemonkey)
 // Released under WTFPL (http://www.wtfpl.net/txt/copying/)
 
+// Note that currently avatars in the chat sidebar and notification drop-down
+// will not animate because iframes suck.
+
 // ==UserScript==
 // @name G+ Animated Avatars
 // @description Animate avatars on Google+
@@ -10,15 +13,32 @@
 // @match https://plus.google.com/*
 // ==/UserScript==
 
+// Change any of these to false to disable animation for their respective cases.
+var animate48px = true; // normal posts
+var animate46px = true; // profile floating banner
+var animate32px = true; // comments and reshares
+var animate24px = true; // post activity / "who +1'd this comment"
+
+var sizes = [];
+if (animate48px) { sizes.push("48"); }
+if (animate46px) { sizes.push("46"); }
+if (animate32px) { sizes.push("32"); }
+if (animate24px) { sizes.push("24"); }
+
+pattern = "googleusercontent\.com(.*)s(" + sizes.join('|') + ")-c-k";
+console.log(pattern);
+var regex = new RegExp(pattern);
+
 function animate() {
     var images = document.getElementsByTagName('img');
     for(i=0; i<images.length; i++) {
-        if (images[i].src.match(/googleusercontent\.com(.*)s(48|46|32|24)-c-k/) != null) {
-            // the test keeps animations from restaring on each call
+        if (images[i].src.match(regex) != null) {
+            // the test keeps animations from restarting on each call
             images[i].src = images[i].src.replace(/s([0-9]+)-c-k/, "s$1-c");
         }
     }
 }
 
 window.addEventListener('load', animate, false);
+var contentPane = document.getElementById('contentPane');
 document.getElementById('contentPane').addEventListener('DOMNodeInserted', animate, false);
