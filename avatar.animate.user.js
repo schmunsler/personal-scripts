c528c1b38be9648e1c0af8c185b345b671482bc3
Cory Schmunsler <schmunsler@gmail.com>
2013-04-21 19:05:25 -0500

    Animate dynamically loaded images.

--- avatar.animate.user.js
+++ avatar.animate.user.js
@@ -10,9 +10,15 @@
 // @match https://plus.google.com/*
 // ==/UserScript==
 
-window.addEventListener('load', function() {
-   var images = document.getElementsByTagName('img');
-   for(i=0; i<images.length; i++) {
-      images[i].src = images[i].src.replace(/googleusercontent\.com(.*)s(48|32|24)-c-k/, "googleusercontent\.com$1s$2-c");
-   }
-}, false);
+function animate() {
+    var images = document.getElementsByTagName('img');
+    for(i=0; i<images.length; i++) {
+        if (images[i].src.match(/googleusercontent\.com(.*)s(48|32|24)-c-k/) != null) {
+            //keeps animation from restaring each call
+            images[i].src = images[i].src.replace(/googleusercontent\.com(.*)s(48|32|24)-c-k/, "googleusercontent\.com$1s$2-c");
+        }
+    }
+}
+
+window.addEventListener('load', animate, false);
+document.getElementById('contentPane').addEventListener('DOMNodeInserted', animate, false);
